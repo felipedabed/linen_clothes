2d20166e9bdd0f1a48208a8a17856df6ca2c07ec
felipedabed <57273007+felipedabed@users.noreply.github.com>
2025-09-13 18:06:14 -0300

    Add files via upload

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,7 @@
-
-/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
-  serverBuildPath: 'server/index.js',
-  serverModuleFormat: 'cjs'
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "build",
+  ignoredRouteFiles: ["**/.*"]
 };
